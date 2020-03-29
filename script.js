24ca381bd254ac688dc6da0e564177d25d5a597d
Marcelo Gumiero <mgumiero9@gmail.com>
2020-03-29 00:27:11 -0300

    counters

--- script.js
+++ script.js
@@ -8,12 +8,33 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
+let cCounter = 0
 
 function createListItem(todo) {
+  itemCountSpan.innerText++
+  cCounter = uncheckedCountSpan.innerText
+  cCounter++
   let children = list.children.length++
   let li = document.createElement('li')
+  let checkbox = document.createElement('input')
+  let label = document.createElement('label')
+  let itemButton = document.createElement('button')
+  checkbox.setAttribute('type', 'checkbox')
+  checkbox.setAttribute('onClick', 'if (this.checked) {' +
+          'cCounter--' +
+      '} else {' +
+          'cCounter++' +
+      '}; ' +
+      'uncheckedCountSpan.innerText = cCounter.toString()'
+  )
+  uncheckedCountSpan.innerText = cCounter.toString()
+  label.innerText = todo;
+
   li.setAttribute('id', `element${children}`)
-  li.appendChild(document.createTextNode(todo));
+  li.appendChild(checkbox)
+  li.appendChild(label)
+  li.appendChild(itemButton)
+
   list.appendChild(li)
 }
 
